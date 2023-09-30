4ba009ce0d548c0c4f9b5ddc9f0a5a7ff6d08f09
Sebastian Lejman <sebastian.lejman21@gmail.com>
2023-09-30 19:48:50 +0000

    Templates for conversation with a mother

--- src/langchain-test.py
+++ src/langchain-test.py
@@ -2,7 +2,7 @@ from langchain.llms import HuggingFaceHub
 from langchain.prompts import PromptTemplate, ChatPromptTemplate
 from langchain.chains import LLMChain
 from dotenv import load_dotenv
-from langchain.schema import HumanMessage
+from langchain.schema import HumanMessage, AIMessage
 import os
 from langchain.schema import BaseOutputParser
 
@@ -16,8 +16,6 @@ class CommaSeparatedListOutputParser(BaseOutputParser):
 
 load_dotenv("/home/slejman/hackyeah_2023/src/keyes.env")
 
-text = "What would be a good company name for a company that makes colorful socks?"
-messages = [HumanMessage(content=text)]
 
 question = "I want to know the best universities in Poland?"
 
@@ -26,15 +24,21 @@ Message: {question}
 
 Answer: """
 
+template = """You are an employment advisor. Based on the message ask for details extensively like you would ask a mother trying to help her son
+Message: {question}
+
+Answer: """
+
 prompt = PromptTemplate(template=template, input_variables=["question"])
+
 repo_id = "google/flan-t5-xxl"
 llm = HuggingFaceHub(
-    repo_id=repo_id, model_kwargs={"temperature": 0.3, "max_length": 64},
+    repo_id=repo_id, model_kwargs={"temperature": 0.2, "max_length": 64},
     huggingfacehub_api_token=os.getenv("huggingfacehub_api_token")
 )
 llm_chain = LLMChain(prompt=prompt, llm=llm)
 print(llm_chain.run(question))
-print(llm.predict_messages(messages).content)
+#print(llm.predict_messages(messages).content)
 
 
 #hf = HuggingFaceHub(repo_id="openlm-research/open_llama_7b_v2", huggingfacehub_api_token="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")
