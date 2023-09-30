5fb470c19d6c6e692a92a52d68cd754ceaefa0cc
Sebastian Lejman <sebastian.lejman21@gmail.com>
2023-09-30 19:22:25 +0000

    Working prediction of mother/student

--- src/langchain-test.py
+++ src/langchain-test.py
@@ -1,16 +1,30 @@
 from langchain.llms import HuggingFaceHub
-from langchain.prompts import PromptTemplate
+from langchain.prompts import PromptTemplate, ChatPromptTemplate
 from langchain.chains import LLMChain
 from dotenv import load_dotenv
+from langchain.schema import HumanMessage
 import os
+from langchain.schema import BaseOutputParser
+
+class CommaSeparatedListOutputParser(BaseOutputParser):
+    """Parse the output of an LLM call to a comma-separated list."""
+
+
+    def parse(self, text: str):
+        """Parse the output of an LLM call."""
+        return text.strip().split(", ")
 
 load_dotenv("/home/slejman/hackyeah_2023/src/keyes.env")
 
-question = "What is the freezing point of water?"
+text = "What would be a good company name for a company that makes colorful socks?"
+messages = [HumanMessage(content=text)]
 
-template = """Question: {question}
+question = "I want to know the best universities in Poland?"
 
-Answer: Let's think step by step."""
+template = """Based on the message determine whether you talk to a mother or to a student
+Message: {question}
+
+Answer: """
 
 prompt = PromptTemplate(template=template, input_variables=["question"])
 repo_id = "google/flan-t5-xxl"
@@ -20,6 +34,8 @@ llm = HuggingFaceHub(
 )
 llm_chain = LLMChain(prompt=prompt, llm=llm)
 print(llm_chain.run(question))
+print(llm.predict_messages(messages).content)
+
 
 #hf = HuggingFaceHub(repo_id="openlm-research/open_llama_7b_v2", huggingfacehub_api_token="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")
 #print(hf.predict("Hi! What are you doing?"))
